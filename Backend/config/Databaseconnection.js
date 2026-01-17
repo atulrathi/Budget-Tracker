e2321defb7675a385c6d0d0dbaa7bfeebaa8ef4e
atul rathi <rathiatul43@gmal.com>
2026-01-17 11:06:04 +0530

    change the dtabase connection

--- Backend/config/Databaseconnection.js
+++ Backend/config/Databaseconnection.js
@@ -1,13 +1,27 @@
 const mongoose = require("mongoose");
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected ✅");
-  } catch (err) {
-    console.error("MongoDB connection failed ❌");
-    process.exit(1);
+  if (cached.conn) {
+    return cached.conn;
   }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(process.env.MONGO_URI, {
+        bufferCommands: false,
+      })
+      .then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  console.log("MongoDB Connected ✅");
+  return cached.conn;
 };
 
 module.exports = connectDB;
